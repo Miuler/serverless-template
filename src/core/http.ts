4883ca30ad5bb0bc742c32b50a47eefce065eaac
Hector Miuler Malpica Gallegos <miuler@gmail.com>
2020-05-14 02:44:32 -0500

    Refactor por eslint

--- src/core/http.ts
+++ src/core/http.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export const get = axios.get;
-export const post = axios.post;
-export const put = axios.put;
-export const patch = axios.patch;
+export const { get } = axios;
+export const { post } = axios;
+export const { put } = axios;
+export const { patch } = axios;
 // export const delete = axios.delete;
--- src/core/logger.ts
+++ src/core/logger.ts
@@ -1,12 +1,15 @@
 import pino from 'pino';
 
-export function getLogger(name: string) {
+export function getLogger(logger: string) {
   let prettyPrint: pino.PrettyOptions;
+  let level: string;
   if ((process.env.IS_LOCAL) || (process.env.AWS_LAMBDA_FUNCTION_NAME !== undefined)) {
     prettyPrint = { colorize: true };
+    level = 'debug';
   }
   return pino({
     prettyPrint,
-    logger: name,
+    level, // warn
+    logger,
   });
 }
